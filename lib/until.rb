0f0823c1caa1161a680c1415022d82e729e8d86b
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-09-12 05:05:31 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
   levitation_force = 6
   #your code here
-    
+    loop do 
+      puts 'Wingardium Leviosa'
+      counter + 1 
+
+      if counter > 10 
+        break
+      end
 end
 
